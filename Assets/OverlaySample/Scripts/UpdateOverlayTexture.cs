e767d780d40e81b2762340cea8a2c70cba5a7450
ycfang22 <46362299+picoxr@users.noreply.github.com>
2022-08-30 17:26:23 +0800

    8.30 update

--- Assets/OverlaySample/Scripts/UpdateOverlayTexture.cs
+++ Assets/OverlaySample/Scripts/UpdateOverlayTexture.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.XR.PXR;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// 刷新RT图至Hole、Overlay
@@ -11,24 +8,11 @@ public class UpdateOverlayTexture : MonoBehaviour
     [SerializeField]
     private Camera m_uiCamera;
 
-    [SerializeField]
-    private MeshRenderer m_hole;
-
-    
-    private  PXR_OverLay m_overlay;
-
     void Start()
     {
-        ControllerPhysicRaycastManager.Instance.RT_Size =
-            new Vector2(m_uiCamera.targetTexture.width, m_uiCamera.targetTexture.height);
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        m_hole.material.mainTexture = m_uiCamera.targetTexture;
-        m_overlay.SetTexture(m_uiCamera.targetTexture,false);
+        if (m_uiCamera != null && m_uiCamera.targetTexture != null)
+        {
+            ControllerPhysicRaycastManager.Instance.RT_Size = new Vector2(m_uiCamera.targetTexture.width, m_uiCamera.targetTexture.height);
+        }
     }
 }
